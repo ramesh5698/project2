540b870027a5b5a1c502a90d4a16c0f195ccc097
ramesh425 <n42785055@gmail.com>
2024-01-29 22:00:47 +0530

    now10

--- src/App.jsx
+++ src/App.jsx
@@ -1,12 +1,9 @@
 import React, { Component } from 'react'
-import Transparent from './Component2/WebapiUpdate'
-import Componentcomp from './Component2/ComponentUpdate'
-import FunctionCall from './Component4/FunctionCall'
-import CardComp from './Componet5/CardComp'
+// import Transparent from './Component2/WebapiUpdate'
+// import Componentcomp from './Component2/ComponentUpdate'
+// import FunctionCall from './Component4/FunctionCall'
 // import 'bootstrap/dist/css/bootstrap.min.css';
-import FuncallApi from './Component4/FuncallApi';
-import Hookscomp from './Componet5/Hookscomp'
-import Useffect from './Componet5/Useffect'
+// import FuncallApi from './Component4/FuncallApi';
 
 
 
